2513693804c51fea40671618acd5bba8367ba3f3
songlairui <songlairui@126.com>
2019-10-12 15:27:31 +0800

    chore(css-loader): update configuration schema

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -82,10 +82,11 @@ export default merge.smart(baseConfig, {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              },
               sourceMap: true,
-              importLoaders: 1,
-              localIdentName: '[name]__[local]__[hash:base64:5]'
+              importLoaders: 1
             }
           }
         ]
@@ -118,10 +119,11 @@ export default merge.smart(baseConfig, {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              },
               sourceMap: true,
-              importLoaders: 1,
-              localIdentName: '[name]__[local]__[hash:base64:5]'
+              importLoaders: 1
             }
           },
           {

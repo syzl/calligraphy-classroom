5c6df9b3e2d2aaa71ed2d73eaa0335d77602c334
songlairui <songlairui@126.com>
2019-10-12 15:28:52 +0800

    feat(react-hot-loader): change appcontainer to hot api

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    const { children } = this.props;
-    return <React.Fragment>{children}</React.Fragment>;
-  }
+export default function App({ children }) {
+  return <React.Fragment>{children}</React.Fragment>;
 }

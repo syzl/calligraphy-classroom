fd55977ba57ca61190af2fe0fefb2dd481e32680
宋来瑞 <cnre@live.cn>
2019-11-06 10:08:57 +0800

    feat(now): execute in the sub directory

--- renderer/lib/constant.ts
+++ renderer/lib/constant.ts
@@ -1,7 +1,9 @@
 const HOST = 'localhost';
 const PORT = '3003';
 
-export const SERVER_URL = `http://${HOST}:${PORT}`;
-export const WS_SERVER_URL = `ws://${HOST}:${PORT}`;
-export const GQL_URI = `http://${HOST}:${PORT}/graphql`;
-export const GQL_WS_URI = `http://${HOST}:${PORT}/graphql`;
+export const SERVER_URL = process.env.SERVER_URL || `http://${HOST}:${PORT}`;
+export const WS_SERVER_URL =
+  process.env.WS_SERVER_URL || `ws://${HOST}:${PORT}`;
+export const GQL_URI = process.env.GQL_URI || `http://${HOST}:${PORT}/graphql`;
+export const GQL_WS_URI =
+  process.env.GQL_WS_URI || `http://${HOST}:${PORT}/graphql`;

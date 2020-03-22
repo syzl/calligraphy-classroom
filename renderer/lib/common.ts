0d3dc946b899938d3c248d4f960ffaba5289af31
songlairui <songlairui@bytedance.com>
2020-03-22 20:19:27 +0800

    fix: infinite-scroll 滚动触发

--- renderer/lib/common.ts
+++ renderer/lib/common.ts
@@ -4,6 +4,6 @@ export const holderCardProp: {
   bordered: boolean;
 } = {
   style: { height: '100%', display: 'flex', flexDirection: 'column' },
-  bodyStyle: { flex: 1, overflow: 'auto' },
+  bodyStyle: { flex: 1, overflow: 'auto', display: 'flex', flexDirection: 'column' },
   bordered: false,
 };
